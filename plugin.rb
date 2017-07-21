e9808ee5c5630ab99c45b10c6c96bb2f361e23ab
David Celis <me@davidcel.is>
2017-07-21 13:40:14 -0700

    Bump version to 4.2.0.334
    Signed-off-by: David Celis <me@davidcel.is>

--- plugin.rb
+++ plugin.rb
@@ -1,10 +1,10 @@
 # name: New Relic RPM
 # about: Report performance data to http://www.newrelic.com/
-# version: 4.0.0.332
+# version: 4.2.0.334
 # authors: Sam Saffron, David Celis
 
 ENV['NRCONFIG'] = File.expand_path('../newrelic.yml', __FILE__)
-gem 'newrelic_rpm', '4.0.0.332'
+gem 'newrelic_rpm', '4.2.0.334'
 
 NewRelic::Control.instance.init_plugin config: Rails.configuration
 
