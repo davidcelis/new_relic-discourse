0eca79ba074c7ec145c00304c5a8c37e3ec3d0bc
SoKeCiLLo <sokecillo@gmail.com>
2018-11-07 12:46:06 +0100

    Update version to 5.4.0.347 for new licenses

--- plugin.rb
+++ plugin.rb
@@ -1,10 +1,10 @@
 # name: New Relic RPM
 # about: Report performance data to http://www.newrelic.com/
-# version: 4.5.0.337
+# version: 5.4.0.347
 # authors: Sam Saffron, David Celis
 
 ENV['NRCONFIG'] = File.expand_path('../newrelic.yml', __FILE__)
-gem 'newrelic_rpm', '4.5.0.337'
+gem 'newrelic_rpm', '5.4.0.347'
 
 NewRelic::Control.instance.init_plugin config: Rails.configuration
 
